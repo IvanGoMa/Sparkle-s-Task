2b6afc8b232b701991d827a63e3b24923d0d1bd8
IvanGoMa <99ivangm@gmail.com>
2025-11-18 19:55:13 +0100

    Avances en clase 18/11

---
+++ app/src/main/java/cat/ivha/sparklestask/HomeActivity.kt
@@ -0,0 +1,33 @@
+package cat.ivha.sparklestask
+
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.widget.CalendarView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var cvCalendari: CalendarView
+
+    override fun onCreate(savedInstanceState: Bundle?):{
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.home)
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    fun initListeners(){}
+    fun initComponents(){
+        cvCalendari = findViewById<CalendarView>(R.id.cvCalendari)
+        cvCalendari.minDate = System.currentTimeMillis()
+        cvCalendari.maxDate = System.currentTimeMillis() + 14*24*60*60*1000
+
+
+    }
+    fun initUI(){}
+
+
+}
