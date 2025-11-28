33438ea8351ca2b043d742026da088cc48a693a7
IvanGoMa <99ivangm@gmail.com>
2025-11-28 17:28:31 +0100

    principi de fragment2

---
+++ app/src/main/java/cat/ivha/sparklestask/HomeFragment.kt
@@ -0,0 +1,33 @@
+package cat.ivha.sparklestask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.home) {
+    lateinit var cvCalendari: CalendarView
+
+    override fun onViewCreated(view: View, savedInstanceState:Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        initComponents(view)
+        initListeners()
+        initUI()
+    }
+
+    fun initListeners(){}
+    fun initComponents(view: View){
+        cvCalendari = view.findViewById<CalendarView>(R.id.cvCalendari)
+        cvCalendari.minDate = System.currentTimeMillis()
+        cvCalendari.maxDate = System.currentTimeMillis() + 14*24*60*60*1000
+
+
+    }
+    fun initUI(){}
+
+
+}
