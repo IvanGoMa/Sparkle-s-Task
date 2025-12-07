171e2e9c36341950711eebefef28262ee6d252da
Ivan González <99ivangm@gmail.com>
2025-12-07 13:47:13 +0100

    mejoras

--- app/src/main/java/cat/ivha/sparklestask/Home.kt
+++ app/src/main/java/cat/ivha/sparklestask/Home.kt
@@ -1,12 +1,15 @@
 package cat.ivha.sparklestask
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class Home : AppCompatActivity() {
     lateinit var cvCalendari: CalendarView
+    lateinit var back : Button
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -19,15 +22,24 @@ class Home : AppCompatActivity() {
 
 
 
-    fun initListeners(){}
+    fun initListeners(){
+
+        back.setOnClickListener {
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
+    }
     fun initComponents(){
         cvCalendari = findViewById<CalendarView>(R.id.cvCalendari)
         cvCalendari.minDate = System.currentTimeMillis()
         cvCalendari.maxDate = System.currentTimeMillis() + 14*24*60*60*1000
+        back = findViewById(R.id.back)
 
 
     }
     fun initUI(){}
+
+    
 
 
 }
