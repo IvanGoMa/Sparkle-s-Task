5475ae1ddff5bf3a07e9d9679c84fa102b055c66
Hanna <hnnzpa@email.com>
2025-12-08 11:26:59 +0100

    Botons funcionals, canvis en perfil(nom d'usuari)

--- app/src/main/java/cat/ivha/sparklestask/Perfil.kt
+++ app/src/main/java/cat/ivha/sparklestask/Perfil.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
-lateinit var back : LinearLayout
 class Perfil : AppCompatActivity() {
+
+    lateinit var back : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
