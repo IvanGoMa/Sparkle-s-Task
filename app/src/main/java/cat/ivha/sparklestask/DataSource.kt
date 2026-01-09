53044e6fef7a731b4c7a4601b93c6af6eb59d72b
Hanna <hnnzpa@email.com>
2026-01-09 19:54:29 +0100

    Fet a classe 9/1, rv perfil roto, no se muestran los imagenes, codigo funcional comentado. Arreglado el menu

--- app/src/main/java/cat/ivha/sparklestask/DataSource.kt
+++ app/src/main/java/cat/ivha/sparklestask/DataSource.kt
@@ -3,9 +3,13 @@ package cat.ivha.sparklestask
 object DataSource {
 
     val items: MutableList<Item> = mutableListOf(
-        Item("Collar", "Un objecte per l'oavatar", R.drawable.moneda),
-        Item("Anells", "Un objecte per l'oavatar", R.drawable.perfil_sin_fondo),
-        Item("Arrecades", "Un objecte per l'oavatar",R.drawable.moneda ),
-        Item("Corona", "Un objecte per l'oavatar", R.drawable.perfil_sin_fondo)
+        Item("Collar", "Un objecte per l'avatar", R.drawable.moneda),
+        Item("Anells", "Un objecte per l'avatar", R.drawable.perfil_sin_fondo),
+        Item("Arrecades", "Un objecte per l'avatar",R.drawable.moneda ),
+        Item("Corona", "Un objecte per l'avatar", R.drawable.perfil_sin_fondo),
+        Item("Collar", "Un objecte per l'avatar", R.drawable.moneda),
+        Item("Anells", "Un objecte per l'avatar", R.drawable.perfil_sin_fondo),
+        Item("Arrecades", "Un objecte per l'avatar",R.drawable.moneda ),
+        Item("Corona", "Un objecte per l'avatar", R.drawable.perfil_sin_fondo)
     )
 }
