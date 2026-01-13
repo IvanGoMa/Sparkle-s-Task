b1442ca325df2ebff114b3d1054cf3ca8e1cf601
Hanna <hnnzpa@email.com>
2026-01-13 19:56:20 +0100

    Fet el grid, falta posar bé les imatges i terminar filtre, me quede en fragment de perfil. A clase

--- app/src/main/java/cat/ivha/sparklestask/Item.kt
+++ app/src/main/java/cat/ivha/sparklestask/Item.kt
@@ -1,7 +1,13 @@
 package cat.ivha.sparklestask
 
+enum class Categoria {
+    COLLARS,
+    GORROS,
+    ULLERES
+}
 data class Item (
     val nom: String,
     val desc: String,
-    val img: Int
+    val img: Int,
+    val categoria: Categoria
 )
--- app/src/main/java/cat/ivha/sparklestask/RecyclerPerfil.kt
+++ app/src/main/java/cat/ivha/sparklestask/RecyclerPerfil.kt
@@ -27,20 +27,31 @@ class MyViewHolder (
 }
 
 class MyAdapter(
-    private val items: List<Item>,
+    private val itemsComplets: List<Item>,
     private val onItemClick: (Item) -> Unit
 ) : RecyclerView.Adapter<MyViewHolder>(){
+
+    private var itemsFiltrados = itemsComplets.toList()
+
+    fun filtra(categoria : Categoria?){
+        itemsFiltrados = if (categoria == null){
+            itemsComplets.toList()
+        } else {
+            itemsComplets.filter { it.categoria == categoria }
+        }
+        notifyDataSetChanged() // Actualizar la vista
+    }
+
+    override fun getItemCount(): Int = itemsFiltrados.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsFiltrados[position]
+        holder.bind(item)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.rv_items_perfil, parent, false)
         return MyViewHolder(view, onItemClick)
-    }
-
-    override fun getItemCount(): Int = items.size
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val item = items[position]
-        holder.bind(item)
     }
 }
