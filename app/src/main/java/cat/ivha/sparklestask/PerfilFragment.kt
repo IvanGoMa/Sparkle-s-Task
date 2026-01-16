99842b2a633702c2b234a8b92be3aff01a38c0c1
Hanna <hnnzpa@email.com>
2026-01-16 18:39:09 +0100

    Fet el filtre, falta provar

--- app/src/main/java/cat/ivha/sparklestask/PerfilFragment.kt
+++ app/src/main/java/cat/ivha/sparklestask/PerfilFragment.kt
@@ -7,6 +7,7 @@ import android.widget.LinearLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -16,14 +17,13 @@ import androidx.recyclerview.widget.RecyclerView
 
 class PerfilFragment : Fragment(R.layout.perfil_rv) {
 
-    lateinit var back : LinearLayout
     lateinit var ivHelp : ImageView
     private lateinit var adapter: MyAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var btnCollars: Button
     private lateinit var btnGorros: Button
     private lateinit var btnUlleres: Button
-
+    private var clicat: Boolean = false;
 
 
     override fun onCreateView(
@@ -37,6 +37,10 @@ class PerfilFragment : Fragment(R.layout.perfil_rv) {
         super.onViewCreated(view, savedInstanceState)
         ivHelp = view.findViewById(R.id.ivHelp)
         recyclerView = view.findViewById(R.id.rvItems)
+        btnCollars = view.findViewById(R.id.btnCollars)
+        btnUlleres = view.findViewById(R.id.btnUlleres)
+        btnGorros = view.findViewById(R.id.btnGorros)
+
 
         val items = DataSource.items
         adapter = MyAdapter(
@@ -44,7 +48,7 @@ class PerfilFragment : Fragment(R.layout.perfil_rv) {
             onItemClick = { item ->
                 Toast.makeText(
                     requireContext(),
-                    "Has clicat",
+                    "Has clicat ${item.nom}",
                     Toast.LENGTH_SHORT
                 ).show()
             }
@@ -60,6 +64,21 @@ class PerfilFragment : Fragment(R.layout.perfil_rv) {
         ivHelp.setOnClickListener {
             val intent = Intent(requireContext(), PerfilHelp::class.java)
             startActivity(intent)
+        }
+
+        btnCollars.setOnClickListener {
+            adapter.filtra(if (!clicat) Categoria.COLLARS else null)
+            clicat = !clicat
+        }
+
+        btnUlleres.setOnClickListener {
+            adapter.filtra(if (!clicat) Categoria.ULLERES else null)
+            clicat = !clicat
+        }
+
+        btnGorros.setOnClickListener {
+            adapter.filtra(if (!clicat) Categoria.GORROS else null)
+            clicat = !clicat
         }
     }
 }
