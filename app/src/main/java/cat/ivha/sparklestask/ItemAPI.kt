ef5068d1546c7621b5234d9866aa85f2a84c6173
Hanna <hnnzpa@email.com>
2026-02-02 18:58:28 +0100

    Fet a clase

---
+++ app/src/main/java/cat/ivha/sparklestask/ItemAPI.kt
@@ -0,0 +1,24 @@
+package cat.ivha.sparklestask
+
+class ItemAPI {
+    companion object {
+        private var mItemAPI: ItemService? = null
+
+        @Synchronized
+        fun API(): ItemService {
+            if (mItemAPI == null) {
+
+                val gsondateformat = GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                    .create()
+
+                mItemAPI = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create(gsondateformat))
+                    .baseUrl("https://oracleitic.mooo.com/")
+                    .build()
+                    .create(ItemService::class.java)
+            }
+            return mItemAPI!!
+        }
+    }
+}
